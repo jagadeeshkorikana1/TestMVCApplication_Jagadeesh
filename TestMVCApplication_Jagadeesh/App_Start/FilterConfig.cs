021f44f805171799012afde3d5f993d227bed6ce
JAGADEESH <2058867@cognizant.com>
2025-12-10 03:09:35 +0530

    Add project files.

---
+++ TestMVCApplication_Jagadeesh/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVCApplication_Jagadeesh
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TestMVCApplication_Jagadeesh/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestMVCApplication_Jagadeesh.MvcApplication" Language="C#" %>
